b94102effd207509846148a0fd2ce2c5137d82b8
Aayush Prajapati <aayush.prajapati0430@gmail.com>
2025-05-26 22:06:47 +0545

    tailwind

--- app/page.js
+++ app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2> title app name </h2>
+      <h2 className="text-5xl font-bold"> title app name </h2>
+      <p className="bg-green-400">hello</p>
     </div>
 
   );
